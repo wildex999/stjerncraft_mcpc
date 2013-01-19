PaperMC/Paper

Update calls to DyeColor getData and getByData. Addresses BUKKIT-2786
These two methods are now deprecated and replaced by the strictly
equivalent calls using wool data.

src/main/java/net/minecraft/server/ItemDye.java
@@ -188,7 +188,7 @@ public class ItemDye extends Item {
                     return false;
                 }
 
-                i = (byte) event.getColor().getData();
+                i = (byte) event.getColor().getWoolData();
                 // CraftBukkit end
 
                 entitysheep.setColor(i);

wildex999/stjerncraft_mcpc

src/minecraft/net/minecraft/item/ItemDye.java
@@ -288,7 +288,7 @@ public class ItemDye extends Item
                     return false;
                 }
 
-                var4 = (byte) event.getColor().getData();
+                var4 = (byte) event.getColor().getWoolData();
                 // CraftBukkit end
                 var3.setFleeceColor(var4);
                 --par1ItemStack.stackSize;