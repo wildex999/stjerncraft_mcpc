PaperMC/Paper

Handle beds and doors the same way as other blocks. Fixes BUKKIT-3437 Relates to: BUKKIT-3438

src/main/java/net/minecraft/server/ItemDoor.java
@@ -1,7 +1,5 @@
 package net.minecraft.server;
 
-import org.bukkit.craftbukkit.block.CraftBlockState; // CraftBukkit
-
 public class ItemDoor extends Item {
 
     private Material a;
@@ -85,17 +83,22 @@ public class ItemDoor extends Item {
             flag2 = true;
         }
 
-        CraftBlockState blockState = CraftBlockState.getBlockState(world, i, j, k); // CraftBukkit
         world.suppressPhysics = true;
-        world.setTypeIdAndData(i, j, k, block.id, l);
         // CraftBukkit start
         if (entityhuman != null) {
-            org.bukkit.event.block.BlockPlaceEvent event = org.bukkit.craftbukkit.event.CraftEventFactory.callBlockPlaceEvent(world, entityhuman, blockState, i, j, k);
-
-            if (event.isCancelled() || !event.canBuild()) {
-                event.getBlockPlaced().setTypeIdAndData(blockState.getTypeId(), blockState.getRawData(), false);
+            if(!ItemBlock.processBlockPlace(world, entityhuman, null, i, j, k, block.id, l)) {
+                ((EntityPlayer) entityhuman).playerConnection.sendPacket(new Packet53BlockChange(i, j + 1, k, world));
                 return false;
             }
+
+            if (world.getTypeId(i, j, k) != block.id) {
+                ((EntityPlayer) entityhuman).playerConnection.sendPacket(new Packet53BlockChange(i, j + 1, k, world));
+                return true;
+            }
+
+            world.suppressPhysics = true;
+        } else {
+            world.setTypeIdAndData(i, j, k, block.id, l);
         }
         // CraftBukkit end
         world.setTypeIdAndData(i, j + 1, k, block.id, 8 | (flag2 ? 1 : 0));

wildex999/stjerncraft_mcpc

src/minecraft/net/minecraft/item/ItemDoor.java
@@ -1,10 +1,11 @@
 package net.minecraft.item;
 
-import org.bukkit.craftbukkit.block.CraftBlockState; // CraftBukkit
 import net.minecraft.block.Block;
 import net.minecraft.block.material.Material;
 import net.minecraft.creativetab.CreativeTabs;
 import net.minecraft.entity.player.EntityPlayer;
+import net.minecraft.entity.player.EntityPlayerMP;
+import net.minecraft.network.packet.Packet53BlockChange;
 import net.minecraft.util.MathHelper;
 import net.minecraft.world.World;
 
@@ -119,20 +120,28 @@ public class ItemDoor extends Item
             flag2 = true;
         }
 
-        CraftBlockState blockState = CraftBlockState.getBlockState(world, i, j, k); // CraftBukkit
         world.editingBlocks = true;
-        world.setBlockAndMetadataWithNotify(i, j, k, block.blockID, l);
 
         // CraftBukkit start
         if (entityhuman != null)
         {
-            org.bukkit.event.block.BlockPlaceEvent event = org.bukkit.craftbukkit.event.CraftEventFactory.callBlockPlaceEvent(world, entityhuman, blockState, i, j, k);
-
-            if (event.isCancelled() || !event.canBuild())
+            if (!ItemBlock.processBlockPlace(world, entityhuman, null, i, j, k, block.blockID, l))
             {
-                event.getBlockPlaced().setTypeIdAndData(blockState.getTypeId(), blockState.getRawData(), false);
+                ((EntityPlayerMP) entityhuman).playerNetServerHandler.sendPacketToPlayer(new Packet53BlockChange(i, j + 1, k, world));
                 return false;
             }
+
+            if (world.getBlockId(i, j, k) != block.blockID)
+            {
+                ((EntityPlayerMP) entityhuman).playerNetServerHandler.sendPacketToPlayer(new Packet53BlockChange(i, j + 1, k, world));
+                return true;
+            }
+
+            world.editingBlocks = true;
+        }
+        else
+        {
+            world.setBlockAndMetadataWithNotify(i, j, k, block.blockID, l);
         }
 
         // CraftBukkit end