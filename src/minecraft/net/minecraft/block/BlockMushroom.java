PaperMC/Paper

Preserve source block in mushroom spread event. Fixes BUKKIT-3354

src/main/java/net/minecraft/server/BlockMushroom.java
@@ -48,6 +48,12 @@ public class BlockMushroom extends BlockFlower {
             j1 = j + random.nextInt(2) - random.nextInt(2);
             k1 = k + random.nextInt(3) - 1;
 
+            // CraftBukkit start - preserve source block coordinates
+            int sourceX = i;
+            int sourceY = j;
+            int sourceZ = k;
+            // CraftBukkit end
+
             for (int l1 = 0; l1 < 4; ++l1) {
                 if (world.isEmpty(i1, j1, k1) && this.d(world, i1, j1, k1)) {
                     i = i1;
@@ -66,7 +72,7 @@ public class BlockMushroom extends BlockFlower {
                 BlockState blockState = bworld.getBlockAt(i1, j1, k1).getState();
                 blockState.setTypeId(this.id);
 
-                BlockSpreadEvent event = new BlockSpreadEvent(blockState.getBlock(), bworld.getBlockAt(i, j, k), blockState);
+                BlockSpreadEvent event = new BlockSpreadEvent(blockState.getBlock(), bworld.getBlockAt(sourceX, sourceY, sourceZ), blockState);
                 world.getServer().getPluginManager().callEvent(event);
 
                 if (!event.isCancelled()) {

wildex999/stjerncraft_mcpc

src/minecraft/net/minecraft/block/BlockMushroom.java
@@ -61,6 +61,11 @@ public class BlockMushroom extends BlockFlower
             var8 = par2 + par5Random.nextInt(3) - 1;
             var9 = par3 + par5Random.nextInt(2) - par5Random.nextInt(2);
             var10 = par4 + par5Random.nextInt(3) - 1;
+            // CraftBukkit start - preserve source block coordinates
+            int sourceX = par2;
+            int sourceY = par3;
+            int sourceZ = par4;
+            // CraftBukkit end
 
             for (int var11 = 0; var11 < 4; ++var11)
             {
@@ -82,7 +87,7 @@ public class BlockMushroom extends BlockFlower
                 org.bukkit.World bworld = par1World.getWorld();
                 BlockState blockState = bworld.getBlockAt(var8, var9, var10).getState();
                 blockState.setTypeId(this.blockID);
-                BlockSpreadEvent event = new BlockSpreadEvent(blockState.getBlock(), bworld.getBlockAt(par2, par3, par4), blockState);
+                BlockSpreadEvent event = new BlockSpreadEvent(blockState.getBlock(), bworld.getBlockAt(sourceX, sourceY, sourceZ), blockState);
                 par1World.getServer().getPluginManager().callEvent(event);
 
                 if (!event.isCancelled())