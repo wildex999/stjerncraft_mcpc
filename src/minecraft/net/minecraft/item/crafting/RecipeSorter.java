5f8283f04060ba9771d0374cae062466e934ca58
Agaricus <agaricusb@yahoo.com>
2013-02-08 23:46:40 -0800

    Fix plugins adding recipes violating sorting contract. Closes #238.
    The vanilla RecipeSorter orders recipes by Shapeless/Shaped and then
    by size, but considers other recipes of equal size as identical.
    The new recipe classes added by Forge and mods will only be compared
    by size. This causes a violation of the sorting contract -- to fix,
    an arbitrary key (the string representation of the recipe) is used
    as a last resort for determining the sorting order.
    
    Also add Forge ore recipes to ensure correct shape/shapeless order
    after Bukkit's sorting.

--- src/minecraft/net/minecraft/item/crafting/RecipeSorter.java
+++ src/minecraft/net/minecraft/item/crafting/RecipeSorter.java
@@ -1,5 +1,8 @@
 package net.minecraft.item.crafting;
 
+import net.minecraftforge.oredict.ShapedOreRecipe;
+import net.minecraftforge.oredict.ShapelessOreRecipe;
+
 import java.util.Comparator;
 
 class RecipeSorter implements Comparator
@@ -13,7 +16,21 @@ class RecipeSorter implements Comparator
 
     public int compareRecipes(IRecipe par1IRecipe, IRecipe par2IRecipe)
     {
-        return par1IRecipe instanceof ShapelessRecipes && par2IRecipe instanceof ShapedRecipes ? 1 : (par2IRecipe instanceof ShapelessRecipes && par1IRecipe instanceof ShapedRecipes ? -1 : (par2IRecipe.getRecipeSize() < par1IRecipe.getRecipeSize() ? -1 : (par2IRecipe.getRecipeSize() > par1IRecipe.getRecipeSize() ? 1 : 0)));
+        // MCPC+ start - add mod recipe support
+        //return par1IRecipe instanceof ShapelessRecipes && par2IRecipe instanceof ShapedRecipes ? 1 : (par2IRecipe instanceof ShapelessRecipes && par1IRecipe instanceof ShapedRecipes ? -1 : (par2IRecipe.getRecipeSize() < par1IRecipe.getRecipeSize() ? -1 : (par2IRecipe.getRecipeSize() > par1IRecipe.getRecipeSize() ? 1 : 0)));
+        if ((par1IRecipe instanceof ShapelessRecipes || par1IRecipe instanceof ShapelessOreRecipe) && (par2IRecipe instanceof ShapedRecipes || par2IRecipe instanceof ShapedOreRecipe)) {
+            return 1;
+        } else if ((par2IRecipe instanceof ShapelessRecipes || par2IRecipe instanceof ShapelessOreRecipe) && (par1IRecipe instanceof ShapedRecipes || par1IRecipe instanceof ShapedOreRecipe)) {
+            return -1;
+        } else if (par2IRecipe.getRecipeSize() < par1IRecipe.getRecipeSize()) {
+            return -1;
+        } else if (par2IRecipe.getRecipeSize() > par1IRecipe.getRecipeSize()) {
+            return 1;
+        } else {
+            // MCPC+ - arbitrary comparison key to ensure transitivity requirement satisfied
+            return par2IRecipe.toString().compareTo(par1IRecipe.toString());
+        }
+        // MCPC+ end
     }
 
     public int compare(Object par1Obj, Object par2Obj)
