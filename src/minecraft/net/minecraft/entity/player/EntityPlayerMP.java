edf72a3bf8356327f6dd15b11ec7ce38b53271a9
shakytom <tom.roberts00@gmail.com>
2013-02-20 22:34:38 -0500

    Spigot patch: 0023 Improved tile entity lookup for chunk sending
    Instead of scanning the entire worlds tile entities to find out what tile entities are in the chunk... just use the arraylist of tile entities we already have.
    This results in a good reduction of time spent in player ticking.
    
    https://github.com/EcoCityCraft/Spigot/blob/7db53a4407d49fd69c30ee29ad68499a6fac51b0/CraftBukkit-Patches/0023-Improved-tile-entity-lookup-for-chunk-sending.patch

--- src/minecraft/net/minecraft/entity/player/EntityPlayerMP.java
+++ src/minecraft/net/minecraft/entity/player/EntityPlayerMP.java
@@ -339,14 +339,13 @@ public class EntityPlayerMP extends EntityPlayer implements ICrafting
 
                 if (chunkcoordintpair != null && this.worldObj.blockExists(chunkcoordintpair.chunkXPos << 4, 0, chunkcoordintpair.chunkZPos << 4))
                 {
-                    arraylist.add(this.worldObj.getChunkFromChunkCoords(chunkcoordintpair.chunkXPos, chunkcoordintpair.chunkZPos));
-                    // MCPC - fixes unable to locate sign bug, do not remove
+                    Chunk chunk = this.worldObj.getChunkFromChunkCoords(chunkcoordintpair.chunkXPos, chunkcoordintpair.chunkZPos); // Spigot
+                    arraylist.add(chunk); // Spigot
+                    arraylist1.addAll(chunk.chunkTileEntityMap.values()); // Spigot
                     //BugFix: 16 makes it load an extra chunk, which isn't associated with a player, which makes it not unload unless a player walks near it.
                     //ToDo: Find a way to efficiently clean abandoned chunks.
-                    // MCPC+ - add entities from loaded chunk instead of bounding box, see #677
                     //arraylist1.addAll(((WorldServer)this.worldObj).getAllTileEntityInBox(chunkcoordintpair.chunkXPos * 16, 0, chunkcoordintpair.chunkZPos * 16, chunkcoordintpair.chunkXPos * 16 + 16, 256, chunkcoordintpair.chunkZPos * 16 + 16));
                     //arraylist1.addAll(((WorldServer)this.worldObj).getAllTileEntityInBox(chunkcoordintpair.chunkXPos * 16, 0, chunkcoordintpair.chunkZPos * 16, chunkcoordintpair.chunkXPos * 16 + 15, 256, chunkcoordintpair.chunkZPos * 16 + 15));
-                    arraylist1.addAll(this.worldObj.getChunkFromChunkCoords(chunkcoordintpair.chunkXPos, chunkcoordintpair.chunkZPos).chunkTileEntityMap.values());
                 }
             }
 
