4a5e4c49349157d368886638d24ebe5b21c60ee3
Yariv Livay <yarivlivay@gmail.com>
2013-02-26 20:14:34 +0200

    Spigot patch 0032: BUKKIT-3668 Fix wrong block in BlockDispenseEvent when filling empty bucket
    https://github.com/EcoCityCraft/Spigot/blob/d9c76252eb02f7d0d6bea11efe77e3bb1edb0d99/CraftBukkit-Patches/0032-BUKKIT-3668-Fix-wrong-block-in-BlockDispenseEvent-wh.patch

--- src/minecraft/net/minecraft/dispenser/DispenserBehaviorEmptyBucket.java
+++ src/minecraft/net/minecraft/dispenser/DispenserBehaviorEmptyBucket.java
@@ -47,7 +47,7 @@ final class DispenserBehaviorEmptyBucket extends BehaviorDefaultDispenseItem
         }
 
         // CraftBukkit start
-        org.bukkit.block.Block block = world.getWorld().getBlockAt(i, j, k);
+        org.bukkit.block.Block block = world.getWorld().getBlockAt(par1IBlockSource.getXInt(), j, par1IBlockSource.getZInt());
         CraftItemStack craftItem = CraftItemStack.asCraftMirror(par2ItemStack);
         BlockDispenseEvent event = new BlockDispenseEvent(block, craftItem.clone(), new org.bukkit.util.Vector(0, 0, 0));
 
