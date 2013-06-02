1a3cc0c516d8975abd92384d0c18a02a91a32a72
bloodshot <jdroque@gmail.com>
2013-06-02 15:47:29 -0400

    Ignore vanilla dimensions when using provider canRespawnHere. Fixes #1120

--- src/minecraft/net/minecraft/server/management/ServerConfigurationManager.java
+++ src/minecraft/net/minecraft/server/management/ServerConfigurationManager.java
@@ -589,7 +589,7 @@ public abstract class ServerConfigurationManager
         {
             i = 0;
         }
-        else if (!world.provider.canRespawnHere())
+        else if (!world.provider.canRespawnHere() && i != 0 && i != -1 && i != 1) // ignore vanilla
         {
             i = world.provider.getRespawnDimension(entityplayermp);
         }
