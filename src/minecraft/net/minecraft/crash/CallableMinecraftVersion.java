72cde05b19974f3d6278da74c77a894e9ee51598
Agaricus <agaricusb@yahoo.com>
2013-06-19 21:20:16 -0700

    Improve error message from invalid MCPC+ installation
    If the vanilla Forge jar is (erroneously) extracted into the MCPC+
    jar, probably from an ill-fated attempt to update Forge, critical classes
    will be overwritten and the server will fail to load. The failure
    could occur with NoSuchMethodError(or field/class) anywhere, but our custom
    MCPC+ code is first called in the FML relauncher, so if it is overwritten
    we'll fail with: java.lang.NoSuchMethodError: cpw.mods.fml.relauncher.FMLInjectionData.obf151()Z
    
    This commit detects this error and shows a message to the user asking
    to retest with a clean MCPC+ jar, as a helpful hint in the right direction.

--- src/minecraft/net/minecraft/crash/CallableMinecraftVersion.java
+++ src/minecraft/net/minecraft/crash/CallableMinecraftVersion.java
@@ -17,7 +17,14 @@ public class CallableMinecraftVersion implements Callable
      */
     public String minecraftVersion()
     {
-        if (cpw.mods.fml.relauncher.FMLInjectionData.obf151()) return "1.5.1"; // MCPC+
+        // MCPC+ start - compatibility mode detection + overwrite detection
+        try {
+            if (cpw.mods.fml.relauncher.FMLInjectionData.obf151()) return "1.5.1";
+        } catch (NoSuchMethodError ex) {
+            System.out.println("SERIOUS ERROR OCCURRED: Critical classes were overwritten! This is an invalid installation. MCPC+ cannot continue. Please retry with a clean, unmodified, MCPC+ server jar.");
+            System.exit(1);
+        }
+        // MCPC+ end
         return "1.5.2";
     }
 
