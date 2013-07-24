46b2be2759b7897c9c6636453c5745297ff9049d
bloodshot <jdroque@gmail.com>
2013-07-24 19:02:24 -0400

    Added missing class BannedItem from previous commit.

--- src/minecraft/cpw/mods/fml/common/registry/BannedItem.java
+++ src/minecraft/cpw/mods/fml/common/registry/BannedItem.java
@@ -1,11 +1,11 @@
 package cpw.mods.fml.common.registry;
 
-public class BannedBlock {
+public class BannedItem {
 
     public final int blockID;
     public final int meta;
 
-    public BannedBlock(int blockID, int meta)
+    public BannedItem(int blockID, int meta)
     {
         this.blockID = blockID;
         this.meta = meta;
